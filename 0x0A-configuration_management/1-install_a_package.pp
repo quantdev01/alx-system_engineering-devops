51f95c1f8678fb9a9dfa2b8012583b15034088ed
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-05-04 02:49:54 +0200

    Trying question 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,8 +3,5 @@
 exec { 'install_flask':
   command => '/usr/bin/pip3 install flask==2.1.0',
   path    => '/usr/local/bin',
-  user    => 'root',
-  group   => 'root',
   creates => '/usr/local/lib/python3.8/site-packages/flask',
 }
-
