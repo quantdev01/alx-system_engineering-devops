8dc6c74bb9a9120ec6addb90dca7fe4993ba2504
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-05-07 01:52:55 +0200

    Good question 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # Installing a package
 
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask',
+  command => '/usr/bin/pip3 install --upgrade Flask Werkzeug',
   path    => '/usr/local/bin',
   creates => '/usr/local/lib/python3.8/site-packages/flask',
 }
