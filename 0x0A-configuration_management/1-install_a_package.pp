c58ccc45a436ff7757a453f31f90b36689ba8a82
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-05-07 02:02:42 +0200

    works now putted the 2 packages

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 # Installing a package
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install --upgrade Flask Werkzeug',
-  path    => '/usr/local/bin',
-  creates => '/usr/local/lib/python3.8/site-packages/flask',
+package { ['Flask', 'Werkzeug']:
+	ensure => '2.1.0',
+	provider => 'pip3',
 }
