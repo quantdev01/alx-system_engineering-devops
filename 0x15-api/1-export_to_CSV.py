ab2b5d3ccef229600a0ef25f65ed483be1925e32
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-06-04 03:11:02 +0200

    Pycodestyle working on it

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -29,11 +29,11 @@ if __name__ == "__main__":
     csv_filename = argv[1] + '.csv'
     with open(csv_filename, mode='w') as f:
         writer = csv.writer(f,
-            delimiter=',',
-            quotechar='"',
-            quoting=csv.QUOTE_ALL)
+                            delimiter=',',
+                            quotechar='"',
+                            quoting=csv.QUOTE_ALL)
         for task in tasks:
             writer.writerow([user['id'],
-            user['username'],
-            task['completed'],
-            task['title']])
+                            user['username'],
+                            task['completed'],
+                            task['title']])
