17175adebaf34ab05568b458f4510b906af8fc0d
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-06-20 02:06:49 +0200

    Working now Task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,28 @@
+# 0-strace_is_your_friend.pp
+#
+# This Puppet manifest ensures Apache and PHP are installed and configures the server to fix the 500 error.
+
+package { 'apache2':
+  ensure => installed,
+}
+
+package { 'php5':
+  ensure => installed,
+}
+
+service { 'apache2':
+  ensure     => running,
+  enable     => true,
+  subscribe  => Package['apache2'],
+}
+
+file { '/var/www/html/index.php':
+  ensure  => file,
+  content => '<?php echo "Hello, World!"; ?>',
+  require => Package['apache2'],
+}
+
+service { 'apache2':
+  ensure => running,
+  enable => true,
+}
