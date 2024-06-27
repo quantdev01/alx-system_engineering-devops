41448f3171c0386e3a58891cbbeca1058218193a
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-06-28 01:13:59 +0200

    work maybe not fixing 0-the sky linter

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,8 @@
 # 0-the_sky_is_the_limit_not.pp
 
 exec { 'increase-nginx-limits':
-  command => '/bin/sed -i "s/worker_connections [0-9]*/worker_connections 2048/" /etc/nginx/nginx.conf && /bin/sed -i "s/keepalive_timeout [0-9]*/keepalive_timeout 65/" /etc/nginx/nginx.conf',
+  command => '/bin/sed -i "s/worker_connections [0-9]*/worker_connections 2048/" /etc/nginx/nginx.conf && /bin/sed -i \
+"s/keepalive_timeout [0-9]*/keepalive_timeout 65/" /etc/nginx/nginx.conf',
   onlyif  => '/usr/bin/grep -q "worker_connections 768" /etc/nginx/nginx.conf',
   notify  => Exec['restart-nginx'],
 }
