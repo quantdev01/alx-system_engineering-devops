8de055f653e1c6f08a55dba0f81f4e06b32f168f
Daniel Kiyiki <danielkiyiki00@gmail.com>
2024-07-01 11:40:50 +0200

    Task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,14 @@
-# 0-the_sky_is_the_limit_not.pp
+# Increases the amount of traffic an Nginx server can handle.
+# By: maryprecious
 
-exec { 'increase-nginx-limits':
-  command => '/bin/sed -i "s/worker_connections [0-9]*/worker_connections 2048/" /etc/nginx/nginx.conf && /bin/sed -i \
-"s/keepalive_timeout [0-9]*/keepalive_timeout 65/" /etc/nginx/nginx.conf',
-  onlyif  => '/usr/bin/grep -q "worker_connections 768" /etc/nginx/nginx.conf',
-  notify  => Exec['restart-nginx'],
-}
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
 
-exec { 'restart-nginx':
-  command     => '/usr/sbin/service nginx restart',
-  refreshonly => true,
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
